8874dc10846056f9395037105cdddb41b9e428cf
Aulia Azzahra <auliazzahraa28@gmail.com>
2025-09-03 22:04:34 +0700

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ import { readFileSync } from "fs";
 dotenv.config();
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
