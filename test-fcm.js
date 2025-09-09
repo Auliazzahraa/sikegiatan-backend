da20fb45d0da3f3b11812c6b4fde5437e6d0e128
Aulia Azzahra <auliazzahraa28@gmail.com>
2025-09-09 10:55:25 +0700

    Update test-fcm.js

--- test-fcm.js
+++ test-fcm.js
@@ -1,22 +1,27 @@
 import admin from "firebase-admin";
+import { getMessaging, send } from "firebase-admin/messaging";
 
+// Ambil service account dari ENV
 const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG);
 
+// Initialize Firebase Admin SDK
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
 
 async function testFCM() {
   const testToken = "f_fBmu4LrMlABYfuE26OSC:APA91bHGP4QJm3ZqSz8suSsfHuznePv3BPsviM29FWzU0iRLPtfgUop-rjmcFT4KH9TJIpLCaKX59MJsW3yfm29HrkeJMrnCqRy7uoqiTwBJ7JmnBMGJYJM";
-  const payload = {
+
+  const message = {
+    token: testToken,
     notification: {
-      title: "Test FCM",
-      body: "Ini test notif"
+      title: "Test Notifikasi",
+      body: "Ini notif test dari server Railway",
     },
   };
 
   try {
-    const response = await admin.messaging().sendToDevice(testToken, payload);
+    const response = await send(getMessaging(), message);
     console.log("✅ FCM berhasil:", response);
   } catch (err) {
     console.error("❌ FCM gagal:", err);
