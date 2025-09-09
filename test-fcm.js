2d4b2a0635e50fff487f5767cf5d98af66d470ad
Aulia Azzahra <auliazzahraa28@gmail.com>
2025-09-09 13:49:22 +0700

    updateeee

--- test-fcm.js
+++ test-fcm.js
@@ -1,6 +1,6 @@
 import admin from "firebase-admin";
 
-const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG.replace(/\\n/g, '\n').trim()
+const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
